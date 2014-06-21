3307a6b9e145ba40d7784f4e6e034844a58eec14
James B Kemp <james@bibble.com>
2014-06-21 12:05:37 +0100

    first commit, sinatra 3 pages written + variables

---
+++ lib/main.rb
@@ -0,0 +1,34 @@
+require 'sinatra'
+
+set :views, Proc.new {File.join(root, '..', "views")}
+# set :public, Proc.new {File.join(root, '..', "public")} is this needed???
+set :public_folder, 'public'
+
+not_found do
+  erb :not_found
+end
+
+get '/' do
+	@title = "Songds by Sinatra - Home"
+  erb :home
+end
+
+get '/about' do
+	@title = "All About This Website"
+  erb :about
+end
+
+get '/contact' do
+	@title = "Contcat us"
+  erb :contact
+end
+
+
+
+
+
+
+
+
+
+
